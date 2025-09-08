8058f15cfe4bd0f6f05b5083f8c20369911f0c9e
patricklarocque1 <patrick.adrian1214@gmail.com>
2025-09-08 10:50:31 -0400

    chore: add yamllint config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 // Root build, keeping it minimal. Module-specific configs live in each module.
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
+    id("com.android.application") version libs.versions.agp.get() apply false
+    kotlin("android") version libs.versions.kotlin.get() apply false
     id("java-library")
 }
 
